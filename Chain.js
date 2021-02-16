239308d7f9606da766e2c9c2eb8a1e74f2feb7eb
raksana-manickam <raaxaanaaa@gmail.com>
2021-02-16 16:57:03 +0530

    class is created

---
+++ Chain.js
@@ -0,0 +1,13 @@
+class Chain {
+    constructor(bodyA, bodyB){
+        var options = {bodyA : bodyA, bodyB : bodyB, stiffness : 0.04, length : 10}
+        this.chain = Constraint.create(options)
+        World.add(world, this.chain)
+    }
+
+    display(){
+        var l = this.chain.bodyA.position
+        var w = this.chain.bodyB.position
+        line(l.x, l.y, w.x, w.y)
+    }
+}
